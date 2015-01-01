c2eb9b0e72ecb875859aaea9c1a14ffcde9dc76b
Deshi Basara <simon@i-was-perfect.net>
2015-01-01 22:54:30 +0100

    Setup description changed

--- cli/setup.py
+++ cli/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="gdocker-ci",
     version="0.0.1",
-    description="Cli prototype for continuous integration with the google cloud platform",
+    description="Cli prototype for deployment with the google cloud platform",
     author="Simon Schuster",
     author_email="simon.schuster@hs-furtwangen.de",
     url="https://github.com/deshi-basara/docker-googlecloud-ci",
