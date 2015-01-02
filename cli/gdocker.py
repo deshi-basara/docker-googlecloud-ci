6959d274ed2c464b20eb78bf32998238bc0fbd8a
Deshi Basara <simon@i-was-perfect.net>
2015-01-02 13:05:18 +0100

    Firewall rules added, progress indicator updated

--- cli/gdocker.py
+++ cli/gdocker.py
@@ -15,7 +15,6 @@ class Config(object):
         self.config = SafeConfigParser()
         self.name = '.gdocker.ini'
         self.verbose = True
-        self.deploy_cmd = []
 
     def create_config(self):
         # create a new '.gdocker.ini'
@@ -33,40 +32,84 @@ class Config(object):
         if self.verbose:
             click.echo('Verbose: Added to .gdocker.ini [%s=%s]' % (key, value))
 
+    def update_config(self, section, key, value):
+        self.config.read(self.name)
+        self.config.set(section, key, value)
+        if self.verbose:
+            click.echo('Verbose: Updated .gdocker.ini [%s=%s]' % (key, value))
+
     def write_config(self):
         with open(self.name, 'w') as config_file:
             self.config.write(config_file)
             if self.verbose:
                 click.echo('Verbose: Config changes written to .gdocker.ini')
 
-    def build_deploy_cmd(self):
+    def build_container_cmd(self):
         # read the config file
         self.config.read(self.name)
         # start command
-        self.deploy_cmd.append("gcloud")
-        self.deploy_cmd.append("compute")
+        container_cmd = []
+        container_cmd.append("gcloud")
+        container_cmd.append("compute")
         # append command options
-        self.deploy_cmd.append("--project")
-        self.deploy_cmd.append("" + self.config.get("gcloud", "project_id"))
-        self.deploy_cmd.append("instances")
-        self.deploy_cmd.append("create")
-        self.deploy_cmd.append("gdocker-project")
-        self.deploy_cmd.append("--zone")
-        self.deploy_cmd.append("" + self.config.get("gcloud", "zone"))
-        self.deploy_cmd.append("--machine-type")
-        self.deploy_cmd.append("" + self.config.get("gcloud", "machine"))
-        self.deploy_cmd.append("--network")
-        self.deploy_cmd.append("default")
-        self.deploy_cmd.append("--image")
-        self.deploy_cmd.append("" + self.config.get("gcloud", "image"))
+        container_cmd.append("--project")
+        container_cmd.append("" + self.config.get("gcloud", "project_id"))
+        container_cmd.append("instances")
+        container_cmd.append("create")
+        container_cmd.append("gdocker-project")
+        container_cmd.append("--zone")
+        container_cmd.append("" + self.config.get("gcloud", "zone"))
+        container_cmd.append("--machine-type")
+        container_cmd.append("" + self.config.get("gcloud", "machine"))
+        container_cmd.append("--network")
+        container_cmd.append("default")
+        container_cmd.append("--image")
+        container_cmd.append("" + self.config.get("gcloud", "image"))
         # append meta data
-        self.deploy_cmd.append("--metadata")
-        self.deploy_cmd.append("url=" + self.config.get("repo", "url"))
-        self.deploy_cmd.append("commit=")
-        self.deploy_cmd.append("start=" + self.config.get("repo", "startup_script"))
-        self.deploy_cmd.append("--metadata-from-file")
-        self.deploy_cmd.append("google-container-manifest=container.yaml")
-        return self.deploy_cmd
+        container_cmd.append("--metadata")
+        container_cmd.append("url=" + self.config.get("repo", "url"))
+        container_cmd.append("commit=")
+        container_cmd.append("start=" + self.config.get("repo", "startup_script"))
+        #container_cmd.append("--metadata-from-file")
+        #container_cmd.append("google-container-manifest=containers.yaml")
+        return container_cmd
+
+    def build_docker_cmd(self):
+        # read the config file
+        self.config.read(self.name)
+        # start command
+        docker_cmd = []
+        docker_cmd.append("gcloud")
+        docker_cmd.append("compute")
+        docker_cmd.append("ssh")
+        # append command options
+        docker_cmd.append("--project")
+        docker_cmd.append("" + self.config.get("gcloud", "project_id"))
+        docker_cmd.append("--zone")
+        docker_cmd.append("" + self.config.get("gcloud", "zone"))
+        docker_cmd.append("gdocker-project")
+        docker_cmd.append("--command")
+        docker_cmd.append("'sudo docker run -p " + self.config.get("gcloud", "port") + ":1337 -i -t deshibasara/gdocker-nodejs ./src/docker.sh'")
+        return docker_cmd
+
+    def build_firewall_cmd(self):
+        # read the config file
+        self.config.read(self.name)
+        # start command
+        firewall_cmd = []
+        firewall_cmd.append("gcloud")
+        firewall_cmd.append("compute")
+        firewall_cmd.append("firewall-rules")
+        firewall_cmd.append("create")
+        firewall_cmd.append("allow-container-port")
+        # append command options
+        firewall_cmd.append("--project")
+        firewall_cmd.append("" + self.config.get("gcloud", "project_id"))
+        firewall_cmd.append("--description")
+        firewall_cmd.append("Incoming container connection allowed")
+        firewall_cmd.append("--allow")
+        firewall_cmd.append("tcp:" + self.config.get("gcloud", "port"))
+        return firewall_cmd
 
     def does_exist(self):
         return path.isfile(self.name)
@@ -99,11 +142,12 @@ def cli(ctx):
               help="Google cloud machine type")
 @click.option('--gimage', default="container-vm",
               help="Google cloud image that will be used for deployment")
+@click.option('--gport', default="1337",
+              help="Google container port that should be exposed")
 @click.argument('repository_url', required=True)
 @click.argument('google_project_id', required=True)
 @pass_config
-def init(config, repository_url, google_project_id, api_version, startup_script,
-         gzone, gmachine, gimage):
+def init(config, repository_url, google_project_id, api_version, startup_script, gzone, gmachine, gimage, gport):
     """
     Initiates the .gdocker-config-file
     """
@@ -124,6 +168,7 @@ def init(config, repository_url, google_project_id, api_version, startup_script,
     config.set_config("gcloud", "zone", gzone)
     config.set_config("gcloud", "machine", gmachine)
     config.set_config("gcloud", "image", gimage)
+    config.set_config("gcloud", "port", gport)
 
     # write config
     config.write_config()
@@ -145,27 +190,41 @@ def deploy(config, new_api_version, commit):
         click.echo("Error: There is no .gdocker.ini existing, initialise it before deployment")
         sys.exit()
 
+    # change the api version number
+    config.update_config("repo", "api_version", new_api_version)
+    config.write_config()
+
     # get all needed deployment values from the config
-    deploy_cmd = config.build_deploy_cmd()
-    click.echo(deploy_cmd)
+    container_cmd = config.build_container_cmd()
 
     # start the vm
-    deploy_process = subprocess.Popen(deploy_cmd, stdout=subprocess.PIPE)
+    deploy_container_process = subprocess.Popen(container_cmd, stdout=subprocess.PIPE)
     click.echo("======================================================================")
-    click.echo("1/5 Deploying Container VM on Google Cloud Platform ...")
+    click.echo("1/6 Deploying Container VM on Google Cloud Platform ...")
     click.echo("======================================================================")
     # block io until the vm is ready & print output
-    while deploy_process.poll() is None:
-        new_line = deploy_process.stdout.readline()
+    while deploy_container_process.poll() is None:
+        new_line = deploy_container_process.stdout.readline()
+        click.echo(new_line)
+
+    firewall_cmd = config.build_firewall_cmd()
+    click.echo(firewall_cmd)
+    firewall_process = subprocess.Popen(firewall_cmd, stdout=subprocess.PIPE)
+    click.echo("======================================================================")
+    click.echo("2/6 Opening firewall ports on Container VM ...")
+    click.echo("======================================================================")
+    # block io until the firewall is ready & print output
+    while firewall_process.poll() is None:
+        new_line = firewall_process.stdout.readline()
         click.echo(new_line)
 
     # start our docker image on the vm
     click.echo("======================================================================")
-    click.echo("2/5 Deploying Docker Image inside Container VM ...")
+    click.echo("3/6 Deploying Docker Image inside Container VM ...")
     click.echo("======================================================================")
 
-    docker_cmd = ["gcloud", "compute", "ssh", "--zone", "europe-west1-b",
-                  "docker-test-1", "--command", "'sudo docker run -d deshibasara/gdocker-nodejs sh /src/docker.sh'"]
+    docker_cmd = config.build_docker_cmd()
+    click.echo(docker_cmd)
     docker_process = subprocess.Popen(docker_cmd, stdout=subprocess.PIPE)
     # block io until the container is ready & print output
     while docker_process.poll() is None:
